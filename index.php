3f8eadc2f0577e99789f46c0e48520340564dae1
Nelli Nyisztor <nynelli@yahoo.de>
2016-12-15 12:21:09 +0100

    php include test

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <!doctype html>
 <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <meta name="name" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-      <title>Nostalgia date</title>
-      <link rel="stylesheet" href="./css/normalize.css" />
-      <link rel="stylesheet" href="./css/style.css" />
-
-      <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-    </head>
+    <!-- include the head -->
+    <?php include 'php-includes/head.php' ?>
     <body>
       <header>
         <nav>
---
+++ php-includes/head.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="name" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Nostalgia date</title>
+  <link rel="stylesheet" href="./css/normalize.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+</head>
