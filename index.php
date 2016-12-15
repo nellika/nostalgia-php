fa0435aeefb861a39a417cfca54d3f1b8e4fee00
Nelli Nyisztor <nynelli@yahoo.de>
2016-12-15 12:00:37 +0100

    php begins...

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="name" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+      <title>Nostalgia date</title>
+      <link rel="stylesheet" href="./css/normalize.css" />
+      <link rel="stylesheet" href="./css/style.css" />
+
+      <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    </head>
+    <body>
+      <header>
+        <nav>
+          <ul>
+            <li>Pricing and Opening hours</li>
+            <li>The museum</li>
+            <li>About us</li>
+            <li class="log-in">Log in</li>
+            <li>About us</li>
+          </ul>
+        </nav>
+        <span id="trans" onclick='paulTrans()'>Hi Paul</span>
+        <div>Nelllliiiii</div>
+
+
+
+      </header>
+      <script>
+      function paulTrans(){
+        var s = document.getElementById('trans');
+        s.className = 'newClass';
+      }
+
+      </script>
+    </body>
+</html>
