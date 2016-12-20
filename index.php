abcacbc05ddb4d92b1ae7901bdb79e9257b7c353
Nelli Nyisztor <nynelli@yahoo.de>
2016-12-20 15:23:15 +0100

    final layout ready to go

--- index.php
+++ index.php
@@ -16,7 +16,13 @@
         <nav class="flex-nav">
             <div class="logo">
               <a href="#">
-                <img src="img/logo.jpg" alt="The first DDR Motorbike Museum in Berlin">
+                <img
+                sizes="(max-width: 1538px) 40vw, 615px"
+                srcset="
+                img/logo/logo-rethink2-small.png 308w,
+                img/logo/logo-rethink2.png 615w"
+                src="img/logo/logo-rethink2-small.png,img/logo/logo-rethink2.png"
+                alt="The first DDR Motorbike Museum in Berlin">
               </a>
             </div>
             <div class="menu">
@@ -35,8 +41,8 @@
                   <p>ddr-nostalgia@motorrad.de</p>
                   <p>+66 50 343 343</p>
                   <div class="social-connect">
-                    <a href="https://twitter.com/GDR_BikesBerlin">T</a>
-                    <a href="https://www.facebook.com/ErstesBerlinerDDRMotorradmuseum">F</a>
+                    <a href="https://twitter.com/GDR_BikesBerlin"><img src="./img/social/twitter.svg" alt="Check out our Twitter" /></a>
+                    <a href="https://www.facebook.com/ErstesBerlinerDDRMotorradmuseum"><img src="./img/social/fb.svg" alt="Visit our Facebook page" /></a>
                   </div>
                 </li>
               </div>
@@ -55,31 +61,43 @@
                 src="img/Pitty_Entire_elcdax_c_scale,w_1400.jpg"
                 alt="">
           <div class="holo">
-
+            <h2>Search for the perfect</h2>
           </div>
           <div class="holo">
-
+            <p class="slogan">
+              <span>&hellip;history on</span>
+              <span>two wheels</span>
+            </p>
           </div>
         </section>
         <section class="find-tours">
-          <article class="tour">
-            <div class="recording">
-              <audio preload>
-                <source src='audio/0831.ogg'/>
-                <source src='audio/0831.mp3'/>
-                <div>
-                  <a href='audio/0831.ogg'>Download <em>ogg</em> File</a>,
-                  <a href='audio/0831.mp3'>Download <em>mp3</em> File</a>
+          <h2>Sign up for the upcoming tours</h2>
+          <article class="tour-ribbon">
+              <h3 class="tour-date">11.12.</h3>
+              <span class="ribbon-left"></span>
+              <span class="ribbon-right"></span>
+              <div class="tour-details">
+                <div class="recording">
+                  <audio preload>
+                    <source src='audio/0831.ogg'/>
+                    <source src='audio/0831.mp3'/>
+                    <div>
+                      <a href='audio/0831.ogg'>Download <em>ogg</em> File</a>,
+                      <a href='audio/0831.mp3'>Download <em>mp3</em> File</a>
+                    </div>
+                  </audio>
+                  <div class="play-button" role="button" onclick="toggleAudio(this)"></div>
                 </div>
-              </audio>
-              <button onclick="toggleAudio(this)">PL</button>
-            </div>
-            <div class="">
-
-            </div>
+                <div class="tour-time">14:00</div>
+                <div class="guide-name">Ralf</div>
+                <div class="tour-enroll"><a href="buy-a-ticket.php"><img src="./img/ticket.png" alt="Buy a ticket" /></a></div>
+                <div class="tour-lang">
+                  <a href="#"><span class="en"><img src="./img/flags/de.png" alt="Tour available in English" /></span></a>
+                  <a href="#"><span class="de"><img src="./img/flags/gb.png" alt="Tour available in German" /></span></a>
+                </div>
+              </div>
           </article>
         </section>
-
       </main>
 
       <script type="text/javascript">
@@ -89,12 +107,15 @@
 
         function toggleAudio(playButton){
           var audioEl = playButton.previousElementSibling;
+          audioEl.parentNode.parentNode.classList.toggle("show-guide");
+
           if(audioEl.paused){
             audioEl.play();
-            playButton.innerHTML = 'FR';
+            // playButton.innerHTML = 'FR';
+            playButton.classList.toggle("playing");
           }else{
             audioEl.pause();
-            playButton.innerHTML = 'PL';
+            playButton.classList.toggle("paused");
           }
         }
 
