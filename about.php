382e71eede208bca5a676be598466655f091bcc6
Nelli Nyisztor <nynelli@yahoo.de>
2017-01-10 22:24:58 +0100

    prototype link added

--- about.php
+++ about.php
@@ -3,8 +3,8 @@
     <?php include 'php-includes/head.php'; ?>
     <body>
       <?php include 'php-includes/navigation.php'; ?>
-      <main>
-        About
+      <main class="not-finished">
+        <a href="https://invis.io/F79ZBL5EC"><h2>Check out the latest prototype created</h2></a>
       </main>
     </body>
 </html>
